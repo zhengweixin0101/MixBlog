b7a4140ee3132264a896d6a30cf91f014d8cf3dd
Weixin Zheng <zhengweixin0101@outlook.com>
2025-08-03 21:53:20 +0800

    放弃星轨背景，减小占用

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative min-h-screen flex flex-col text-white">
     <Header />
-    <div class="flex flex-1 bg-#0e1111">
+    <div class="flex flex-1">
       <div class="flex-1 w-full flex flex-col px-4">
         <div class="w-full mx-auto flex-1 flex flex-col
                     max-w-full sm:max-w-[90vw] md:max-w-[85vw] lg:max-w-[75vw] 2xl:max-w-[60vw]">
--- src/components/Background.vue
+++ src/components/Background.vue
@@ -1,3 +1,16 @@
 <template>
-
+  <div
+    class="fixed inset-0 -z-10 bg-[#0e1111]"
+  >
+    <div
+      class="absolute w-full h-full pointer-events-none"
+      :style="{
+        backgroundImage: `
+          linear-gradient(to right, rgba(255,255,255,0.05) 1px, transparent 1px),
+          linear-gradient(to bottom, rgba(255,255,255,0.05) 1px, transparent 1px)
+        `,
+        backgroundSize: '15px 15px'
+      }"
+    />
+  </div>
 </template>
