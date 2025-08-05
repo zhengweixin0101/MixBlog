51aafbe6b3ca760c49d3762749dd7737a78c9130
Weixin Zheng <zhengweixin0101@outlook.com>
2025-08-05 20:16:08 +0800

    微调

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="relative min-h-screen flex flex-col text-#2f3f5b dark:text-white transition-colors duration-300">
     <Header />
     <div class="flex flex-1">
-      <div class="flex-1 w-full flex flex-col px-4">
+      <div class="flex-1 w-full flex flex-col">
         <div class="w-full mx-auto flex-1 flex flex-col
                     max-w-full sm:max-w-[90vw] md:max-w-[85vw] lg:max-w-[75vw] 2xl:max-w-[60vw]">
           <div class="flex-1 relative z-10 px-10">
