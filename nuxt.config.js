0be8227bab5b71c58d7b45f9530cdde94966f379
Weixin Zheng <zhengweixin0101@outlook.com>
2025-08-11 16:02:15 +0800

    尝试修复bug

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+  nitro: {
+    publicAssets: [
+      { base: '/posts', dir: 'posts' }
+    ],
+  },
   modules: [
     '@nuxtjs/color-mode',
     '@unocss/nuxt',
