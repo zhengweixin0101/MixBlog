26777c6ad116486c309c12c895afa0f07421d619
Weixin Zheng <zhengweixin0101@outlook.com>
2025-08-12 13:20:14 +0800

    Revert "Update [slug].vue"
    This reverts commit b58c548c2f4f94d886b17ed7c43bf7aab422770b.

--- pages/posts/[slug].vue
+++ pages/posts/[slug].vue
@@ -13,6 +13,8 @@ import '@fancyapps/ui/dist/fancybox/fancybox.css'
 import Sidebar from '@/components/Sidebar.vue'
 import '@/assets/article-content.css'
 
+const route = useRoute()
+
 // HTML 实体解析
 function decodeHTMLEntities(str) {
   if (typeof document === 'undefined') return str
