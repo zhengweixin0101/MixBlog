6b2747f986b16b9deff4a9652c086748f8ecbec6
Weixin Zheng <zhengweixin0101@outlook.com>
2025-08-16 16:28:47 +0800

    微调

--- app.vue
+++ app.vue
@@ -42,7 +42,7 @@ useHead({
 
   script: [
     {
-      innerHTML: `!function(p){!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"${siteConfig.thirdParty.s51LA.id}",ck:"${siteConfig.thirdParty.s51LA.ck}",autoTrack:true,hashMode:true,screenRecord:true});`,
+      innerHTML: `!function(p){!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"${siteConfig.thirdParty.s51LA.id}",ck:"${siteConfig.thirdParty.s51LA.ck}",hashMode:true});`,
       type: "text/javascript"
     }
   ],
