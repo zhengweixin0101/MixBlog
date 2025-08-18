46c612e491b9b7040c2ffd231a1fbcff8ddf7a1c
Weixin Zheng <zhengweixin0101@outlook.com>
2025-08-18 13:40:35 +0800

    完善关于页面

--- siteConfig/about.js
+++ siteConfig/about.js
@@ -2,6 +2,9 @@ export const aboutConfig = {
     author: {
         name: "郑炜鑫",
         description: "是一名 高中学生、游戏玩家、及独立开发者。",
+        birthday: '2010',
+        school: '滨江高级中学',
+        work: '高一学生',
         tips: [
             { text: "学习", color: "#10b981" },
             { text: "生活", color: "#a393eb" },
@@ -9,18 +12,54 @@ export const aboutConfig = {
         ],
         tags: {
             left: [
-                '游戏数码发烧友 🎮',
-                '音乐品味收藏家 🎧',
-                '分享与热心帮助 🔍',
+                '🎮 游戏数码发烧友',
+                '🎧 音乐品味收藏家',
+                '🔍 分享与热心帮助',
             ],
             right: [
-                '🎯 目标明确执行快',
-                '🏃 脚踏实地行动派',
-                '🚀 热衷探索未知域',
+                '目标明确执行快 🎯',
+                '脚踏实地行动派 🏃',
+                '热衷探索未知域 🚀',
             ]
+        },
+        skills: [
+            { name: 'Vite', icon: 'https://cdn.zhengweixin.top/blog/about/skills/vite.svg', color: '#937df7' },
+            { name: 'Vue', icon: 'https://cdn.zhengweixin.top/blog/about/skills/vue.png', color: '#b8f0ae' },
+            { name: 'Nuxt', icon: 'https://cdn.zhengweixin.top/blog/about/skills/nuxt.png', color: '#333' },
+            { name: 'Astro', icon: 'https://cdn.zhengweixin.top/blog/about/skills/astro.svg', color: '#fff' },
+            { name: 'CSS3', icon: 'https://cdn.zhengweixin.top/blog/about/skills/css.png', color: '#2c51db' },
+            { name: 'JS', icon: 'https://cdn.zhengweixin.top/blog/about/skills/js.png', color: '#f7cb4f' },
+            { name: 'HTML', icon: 'https://cdn.zhengweixin.top/blog/about/skills/html.png', color: '#e9572b' },
+            { name: 'Node', icon: 'https://cdn.zhengweixin.top/blog/about/skills/node.svg', color: '#333' },
+            { name: 'Docker', icon: 'https://cdn.zhengweixin.top/blog/about/skills/docker.png', color: '#57b6e6' },
+            { name: 'Python', icon: 'https://cdn.zhengweixin.top/blog/about/skills/python.png', color: '#fff' },
+            { name: 'Git', icon: 'https://cdn.zhengweixin.top/blog/about/skills/git.webp', color: '#df5b40' },
+            { name: 'Photoshop', icon: 'https://cdn.zhengweixin.top/blog/about/skills/ps.png', color: '#4082c3' },
+            { name: 'DJI', icon: 'https://cdn.zhengweixin.top/blog/about/skills/dji.svg', color: '#000' },
+            { name: 'VALORANT', icon: 'https://cdn.zhengweixin.top/blog/about/skills/valorant.svg', color: '#ff4654' },
+        ],
+        preference: {
+            category: '数码科技',
+            description: '手机、电脑软硬件',
+            img: 'https://cdn.zhengweixin.top/blog/about/632f0dd8f33c6.webp',
+        },
+        personality: {
+            name: '冒险家',
+            code: 'ISFP-T',
+            img: 'https://cdn.zhengweixin.top/blog/about/ISFP.svg',
         },
     },
     helloCard: {
         text: "Hello there!",
+        colors: ['#66c6ba', '#a4e5d9', '#c8f4de'],
+    },
+    umami: {
+        cover: 'https://cdn.zhengweixin.top/blog/about/632d634f8376d.jpg',
+        url: 'https://statistics.zhengweixin.top',
+        shareUrl: 'https://statistics.zhengweixin.top/share/6FBrEjH0q8vcrGxh/zhengweixin.top',
+        siteId: '7441ce23-3587-41b6-8919-e42932fc65d7',
+        token: 'nmDTV73ucyUS8829KW3miG8O/obEDyPu1Jey5st9HWUhXRQnA9mP5a9xMjVukSve/we34uIGNszqlwlo6ZrcRIW+OUon3O3/NkepjhxSFmIAAxoKjHqgoopaip+NharfN8egGKfog5Ypv2KAGnxpHtE9tl7NgNh93EbuGApaYcZeN+kmNzTpOfookmYlWkv2+9flKognxoXE/84UZ6Xz8zGUXG5+qPXwSwnk5gQoSuFPcJ1bCuP5V2hEb5i12tgOEORqrtEXzvwhloag+QiDeKQ+8RqluxSThooId4gy9onIstofGISPfRJ8qS9G0v9aC1qqJh/nXyBxfbi8HVeL51iio8M1HXENhQ==',
+        createTime: '2025-08-15T16:00:00.000Z',
+        cacheDuration: 10 * 60 * 1000,
     },
 }
