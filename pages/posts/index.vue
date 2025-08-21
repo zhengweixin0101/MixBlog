43e2020fd3416d2a5457d91ce48da1de287f4e5d
Weixin Zheng <zhengweixin0101@outlook.com>
2025-08-21 18:24:45 +0800

    优化文章列表卡片

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -110,7 +110,7 @@ const filteredPosts = computed(() => {
           :key="post.slug + selectedTag + searchTerm"
           :class="[
             'relative w-full h-160px rounded-xl list-none will-change-transform motion-safe:transform-gpu transition duration-300 animate-shadow',
-            'hover:shadow-[0_0_0_1px_#00e699] hover:scale-[1.02] active:scale-[0.97] shadow-[0_0_2px_rgba(0,0,0,0.2)]'
+            'hover:scale-[1.02] shadow-[0_0_2px_rgba(0,0,0,0.2)]'
           ].join(' ')"
         >
           <NuxtLink
@@ -118,7 +118,9 @@ const filteredPosts = computed(() => {
             class="block h-full rounded-xl no-underline focus:outline-none cursor-pointer"
           >
             <div
-              class="bg-#fefefe dark:bg-white/10 h-full rounded-xl p-4 no-underline focus:outline-none focus-visible:ring focus-visible:ring-[#00e699] transition-transform duration-300 active:scale-95 hover:scale-102"
+              class="bg-#fefefe dark:bg-white/10 h-full rounded-xl p-4 no-underline focus:outline-none
+                    focus-visible:ring focus-visible:ring-[#00e699]
+                    transition-transform duration-300 hover:scale-102 hover:shadow-[0_0_0_1px_#00e699]"
             >
               <h4 class="text-#2f3f5b dark:text-white text-xm transition-colors duration-300">{{ post.title }}</h4>
               <p class="mt-1 text-#2f3f5b dark:text-gray-400 text-sm transition-colors duration-300">
