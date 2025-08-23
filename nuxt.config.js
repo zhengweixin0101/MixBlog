c139c81203c5edc66693bcc49b360af997c17331
Weixin Zheng <zhengweixin0101@outlook.com>
2025-08-23 11:00:41 +0800

    修复bug

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,19 +3,15 @@ import { siteConfig } from './siteConfig/main.js'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   ssr: true,
-  nitro: {
-    prerender: {
-      routes: async () => {
-        const baseRoutes = ['/', '/apps', '/talks', '/about'];
-        if (process.argv.includes('generate')) {
-          const res = await fetch(siteConfig.postsData.postsList)
-          const posts = await res.json()
-          const postRoutes = posts.map(post => `/posts/${post.slug}`)
-          return [...baseRoutes, ...postRoutes]
-        }
-        return baseRoutes
+  hooks: {
+    'nitro:build:before': async (nitro) => {
+      if (nitro.options.prerender.generate) {
+        const res = await fetch(siteConfig.postsData.postsList)
+        const posts = await res.json()
+        nitro.options.prerender.routes = posts.map(post => `/posts/${post.slug}`)
+        console.log('Prerender routes:', nitro.options.prerender.routes)
       }
-    }
+    },
   },
   modules: ['@nuxtjs/color-mode', '@unocss/nuxt'],
   colorMode: {
