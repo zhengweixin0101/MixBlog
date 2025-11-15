47a7c7ffd7e7b2ab94b9c1c92ee3ebac89d5573d
Weixin Zheng <zhengweixin0101@outlook.com>
2025-11-15 19:10:35 +0800

    Update music.js

--- siteConfig/music.js
+++ siteConfig/music.js
@@ -1,6 +1,6 @@
 export const musicConfig = {
     listFile: {
-        basic: 'https://gh-proxy.com/https://raw.githubusercontent.com/zhengweixin0101/CDN/refs/heads/music',
+        basic: 'https://gh-proxy.org/https://raw.githubusercontent.com/zhengweixin0101/CDN/refs/heads/music',
         path: '/music/music_list.json',
     }
 }
