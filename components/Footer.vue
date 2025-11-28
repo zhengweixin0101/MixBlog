5c08f496206c43f351a14135c58a51cb5ee48c36
Weixin Zheng <zhengweixin0101@outlook.com>
2025-11-28 18:17:18 +0800

    优化footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { siteConfig } from '@/siteConfig/main.js'
+
+const currentYear = new Date().getFullYear()
 </script>
 
 <template>
   <div class="w-full h-0.3 bg-#2f3f5b/20 dark:bg-gray-700 my-8 transition-colors duration-300"></div>
   <footer class="mb-10 w-full text-center text-#2f3f5b dark:text-gray-300 text-sm">
-    © 2025 {{ siteConfig.author.name }}. All rights reserved.
+    © {{ currentYear }} {{ siteConfig.author.name }}. All rights reserved.
   </footer>
 </template>
