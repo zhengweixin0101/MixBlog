45f2fa616fba3721ab902345dbcefa9ead0abc25
Weixin Zheng <zhengweixin0101@outlook.com>
2026-07-07 16:09:31 +0800

    Revert "Update music.js"
    This reverts commit 4bb6374d02b3b697620970de562695b33df5eb17.

--- siteConfig/music.js
+++ siteConfig/music.js
@@ -1,6 +1,6 @@
 export const musicConfig = {
     listFile: {
-        basic: 'https://cdn.zhengweixin.top/music',
+        basic: 'https://gh-proxy.org/https://raw.githubusercontent.com/zhengweixin0101/CDN/refs/heads/music',
         path: 'music_list.json',
     }
 }
